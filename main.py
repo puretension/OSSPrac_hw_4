007ce4f4c9fec8378fcd14770c911041c3dcbf8c
puretension <rlrlfhtm5@gmail.com>
2023-09-15 13:51:39 +0900

    2023-09-15 실습과제 #4 3번

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print('hellow world')
+name = input("이름을 입력하세요: ")
+student_id = input("학번을 입력하세요: ")
+department = input("학과를 입력하세요: ")
+
+print('\n<출력>')
+print(f"이름:{name}")
+print(f"학번:{student_id}")
+print(f"학과:{department}")
